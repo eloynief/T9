6878c6531c7231b2144db1e2a7526146958e3fb9
enieves <enieves@106-02.info2>
2023-05-03 14:27:54 +0200

    Help

---
+++ Tema9/src/aplicacion/Main.java
@@ -0,0 +1,39 @@
+package aplicacion;
+
+import java.util.TreeSet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		//pensar como si fuera un array
+		TreeSet<Integer> numeros= new TreeSet<Integer>();
+		
+		
+		
+		for(int i=0;;i++) {
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		/*Aplicacion<Integer> numero= new Aplicacion<Integer>();*/
+		
+		
+		
+		
+		
+		
+	}
+
+}
